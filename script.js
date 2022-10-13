bb613629d2893e44de930488d3090fab2b57a015
muflihanto <muflihanto@gmail.com>
2022-10-13 22:09:50 +0700

    fix layouts, refactor code, add sweetalert2, and write readme

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
-const firstName = document.getElementsByName("first-name")[0];
-const lastName = document.getElementsByName("last-name")[0];
-const email = document.getElementsByName("email")[0];
-const password = document.getElementsByName("password")[0];
-const form = document.getElementsByTagName("form")[0];
+const elementsName = ["first-name", "last-name", "email", "password", "signup-form"];
+const innerText = ["First Name", "Last Name", "Email Address", "Password"];
+const [firstName, lastName, email, password, form] = elementsName.map((el) => {
+  return document.getElementsByName(el)[0];
+});
 
-const getRequired = (text) => {
+const getRequiredAlert = (text) => {
   const required = document.createElement("div");
   required.classList.add("required");
   required.innerText = text + " cannot be empty";
   return required;
 };
 
-const getMailPattern = () => {
+const getEmailPatternAlert = () => {
   const mail = document.createElement("div");
   mail.classList.add("required");
   mail.innerText = "Looks like this is not an email";
@@ -26,9 +26,10 @@ const cleanup = (el) => {
   }
 };
 
-const handleChange = (e) => {
+const handleKeyUp = (e) => {
   const el = document.getElementsByName(e.target.name)[0];
   cleanup(el);
+  el.placeholder = innerText[elementsName.indexOf(e.target.name)];
 };
 
 const requiredValidation = (el, text) => {
@@ -36,7 +37,7 @@ const requiredValidation = (el, text) => {
     el.classList.add("invalid");
     el.placeholder = "";
     el.parentElement.classList.add("label-invalid");
-    el.after(getRequired(text));
+    el.after(getRequiredAlert(text));
   }
 };
 
@@ -47,7 +48,7 @@ const emailValidation = (el, text) => {
   } else if (!/[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/.test(el.value)) {
     el.classList.add("invalid");
     el.parentElement.classList.add("label-invalid");
-    el.after(getMailPattern());
+    el.after(getEmailPatternAlert());
   }
 };
 
@@ -57,11 +58,11 @@ const handleSubmit = (e) => {
   [firstName, lastName, email, password].forEach((el, index) => {
     cleanup(el);
     if (index !== 2) {
-      requiredValidation(el, ["First Name", "Last Name", "Email", "Password"][index]);
+      requiredValidation(el, innerText[index]);
     } else {
-      emailValidation(el, ["First Name", "Last Name", "Email", "Password"][index]);
+      emailValidation(el, innerText[index]);
     }
-    el.addEventListener("change", handleChange);
+    el.addEventListener("keyup", handleKeyUp, { once: true });
   });
 
   let status = true;
@@ -71,8 +72,9 @@ const handleSubmit = (e) => {
   if (status) {
     [firstName, lastName, email, password].forEach((el, index) => {
       el.value = "";
-      el.placeholder = ["First Name", "Last Name", "Email", "Password"][index];
+      el.placeholder = innerText[index];
     });
+    Swal.fire("Congratulations!", "Your account has been successfully created.", "success");
   }
 };
 
